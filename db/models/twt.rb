1c56c979214c85ce13d618f30c7eab91ece2a96f
ShreyanJain9 <shreyan.jain.9@outlook.com>
2023-08-05 22:52:07 -0700

    less broken models (fixed gpt's nonsense)

--- db/models/twt.rb
+++ db/models/twt.rb
@@ -1,5 +1,6 @@
 class Twt < Sequel::Model
-  # ... (existing code remains the same) ...
+  many_to_one(:feed, class: :Feed)
+  one_to_many(:mentions, class: :Mention)
 
   def self.create_from_json(tweet, feed)
     Twt.first(hash: tweet[:hash]).then do |twt|
@@ -10,12 +11,8 @@ class Twt < Sequel::Model
         reply_to: tweet[:twt][:reply_to],
         hash: tweet[:hash],
       ).tap do |twt|
-        begin
-          tweet[:twt][:mentions]&.each do |mention|
-            Feed.from_url(mention[:url]).then { |feed_mention| twt.add_mention(Mention.create(feed: feed_mention.tap(&method(:puts)))) }
-          end
-        rescue => e
-          puts "Error in create_from_json: #{e.message}"
+        tweet[:twt][:mentions]&.each do |mention|
+          Feed.from_url(mention[:url]).then { |feed_mention| twt.add_mention(Mention.create(feed: feed_mention.tap(&method(:puts)))) }
         end
       end
     end
@@ -23,7 +20,18 @@ class Twt < Sequel::Model
 end
 
 class Feed < Sequel::Model
-  # ... (existing code remains the same) ...
+  one_to_many(:twts, class: :Twt)
+  one_to_many(:mentions, class: :Mention)
+
+  def self.from_url(url)
+    Feed.first(url: url).then do |feed|
+      if feed
+        feed
+      else
+        Feed.create(url: url)
+      end
+    end
+  end
 
   def self.from_metadata(metadata)
     Feed.from_url(metadata[:url]).then do |feed|
@@ -35,12 +43,17 @@ class Feed < Sequel::Model
           url: metadata[:url].tap(&method(:puts)),
           avatar: metadata[:avatar],
         ).tap do |db_feed|
-          begin
-            metadata[:follow]&.each do |user|
-              Feed.from_url(user[:url]).then { |follow_feed| Follow.find_or_create(follower_id: db_feed.id, following_id: follow_feed.id) }
-            end
-          rescue => e
-            puts "Error in from_metadata: #{e.message}"
+          metadata[:follow]&.each do |user|
+            Feed.from_url(
+              user[:url]
+            )
+              .then {
+              |follow_feed|
+              Follow.find_or_create(
+                follower_id: db_feed.id,
+                following_id: follow_feed.id,
+              ) unless db_feed[:url] == follow_feed[:url]
+            }
           end
         end
       end
@@ -50,13 +63,10 @@ class Feed < Sequel::Model
   def process
     FetchTwts.from(url).then do |raw|
       Feed.from_metadata(raw[:metadata]).tap do |db_feed|
-        begin
-          raw[:twts].map do |twt|
-            Twt.create_from_json(twt.tap(&method(:puts)), db_feed)
-          end
-        rescue => e
-          puts "Error in process: #{e.message}"
-        end
+        raw[:twts].map {
+          |twt|
+          Twt.create_from_json(twt.tap(&method(:puts)), db_feed)
+        }
       end
     end
   end
